2afa56c6790783d4395b8670d26b24424e07d6dd
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2024-10-05 18:32:21 -0400

    Atualização

--- codigos/Projeto_API_Estoque/estoque/src/main/java/poo2/estoque/service/IBaseServicoJPA.java
+++ codigos/Projeto_API_Estoque/estoque/src/main/java/poo2/estoque/service/IBaseServicoJPA.java
@@ -1,10 +1,11 @@
 package poo2.estoque.service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface IBaseServicoJPA<TDominio> {
     ArrayList<TDominio> Browse();
-    TDominio Read(Long chave);
+    Optional<TDominio> Read(Long chave);
     TDominio Edit(TDominio obj);
     TDominio Add(TDominio obj);
     TDominio Delete(Long chave);
